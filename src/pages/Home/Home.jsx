feb98eae7a3e4e2cdb13833b6a27b0008fe1f823
Mykhailo Syritchenko <syritchenko@gmail.com>
2022-05-04 23:54:17 +0200

    Modified  src/pages/Home/Home.jsx file add httpService and urls

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import AboutSection from '../../components/Content/AboutSection/AboutSection';
 import AdvantagesSection from '../../components/Content/AdvantagesSection/AdvantagesSection';
 import ChlorophyllSection from '../../components/Content/ChlorophyllSection/ChlorophyllSection';
 import WaterSection from '../../components/Content/WaterSection/WaterSection';
 import Header from '../../components/Header/Header';
+import httpService from '../../services/httpService';
 import './Home.scss';
 
+export const urls = {
+	GET_AREAS_PRIMARY: '/areas/primary',
+	GET_AREAS_SECONDARY: '/areas/secondary',
+	GET_AREAS_DATA: '/areas/data'
+};
+
 function Home() {
+	useEffect(() => {
+		httpService.get('/areas/primary');
+		httpService.get('/areas/secondary');
+		httpService.get('/areas/data');
+	}, []);
 	return (
 		<Container className="homeContainer">
 			{/* Import Header */}
