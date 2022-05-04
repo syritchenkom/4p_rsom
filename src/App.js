d66eb34c108f6ca3cb30b955ff62a074f0f53a6d
Mykhailo Syritchenko <syritchenko@gmail.com>
2022-05-05 00:06:35 +0200

    Modified src/App.js add functionality, token and  setAuthorizationToken

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { useState } from 'react';
 import { ThemeProvider } from 'react-bootstrap';
 import Login from './pages/Auth/Login/Login';
 import Home from './pages/Home/Home';
+import { setAuthorizationToken } from './services/httpService';
 
 function App() {
+	const token = localStorage.getItem('authToken');
+	const [isAuth, setIsAuth] = useState(!!token);
+	token && setAuthorizationToken(token);
 	return (
 		<ThemeProvider breakpoints={['xxl', 'md', 'sm']}>
-			{localStorage.getItem('authToken') ? <Home /> : <Login />}
+			{isAuth ? (
+				<Home />
+			) : (
+				<Login
+					isAuthHandler={(value) => {
+						setIsAuth(value);
+					}}
+				/>
+			)}
 		</ThemeProvider>
 	);
 }
