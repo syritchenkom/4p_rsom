cffcd115a23329049bdffffd50d3daf986896147
Mykhailo Syritchenko <syritchenko@gmail.com>
2022-05-06 23:15:34 +0200

    Modified files and add functionality to src/components/Content/WaterSection/WaterSection.jsx

--- src/components/Content/WaterSection/WaterSection.jsx
+++ src/components/Content/WaterSection/WaterSection.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, Container, Dropdown } from 'react-bootstrap';
 import './WaterSection.scss';
-import WaterPng from '../../../assets/contentSection/Water/water.png';
+import SatelliteViewPng from '../../../assets/contentSection/Water/satelliteView.png';
+import WaterLinePng from '../../../assets/contentSection/Water/waterLine.png';
+
+export const Images = {
+	satellite: {
+		img: SatelliteViewPng,
+		alt: 'Satellite View Image'
+	},
+	waterLine: {
+		img: WaterLinePng,
+		alt: 'Water Line Image'
+	}
+};
 
 const WaterSection = () => {
-	// const []
+	const [selectedImg, setSelectedImg] = useState();
 	return (
 		<Container fluid="auto" className="waterContainer">
 			<div className="waterSection">
@@ -12,19 +24,29 @@ const WaterSection = () => {
 			</div>
 			<Dropdown className="waterDropdown">
 				<Dropdown.Toggle
-					// variant="transparent"
+					variant="transparent"
 					id="dropdown-basic"
-					className="dropdownToggle">
-					Satellite view
-				</Dropdown.Toggle>
+					className="dropdownToggle"></Dropdown.Toggle>
 				<Dropdown.Menu className="dropdownMenu">
-					<Dropdown.Item href="#/action-1">Satellite view</Dropdown.Item>
-					<Dropdown.Item href="#/action-2">Waterline </Dropdown.Item>
+					<Dropdown.Item
+						eventKey="1"
+						active
+						onClick={() => setSelectedImg(Images.satellite)}>
+						Satellite view
+					</Dropdown.Item>
+					<Dropdown.Item
+						eventKey="2"
+						onClick={() => setSelectedImg(Images.waterLine)}>
+						Waterline
+					</Dropdown.Item>
 				</Dropdown.Menu>
 			</Dropdown>
+			{/* Take HeaderImage.png */}
+
 			<Card className="waterImg">
-				{/* Take HeaderImage.png */}
-				<Card.Img src={WaterPng} alt="Water Image" />
+				{selectedImg ? (
+					<Card.Img src={selectedImg.img} alt={selectedImg.alt} />
+				) : null}
 			</Card>
 		</Container>
 	);
